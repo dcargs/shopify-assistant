772f8d6a9f53dce79bcba6c670230aa30db09b85
dcargs <cargilldevin@gmail.com>
2020-02-10 00:26:18 -0600

    first commit

---
+++ api/imported_order/order.php
@@ -0,0 +1,19 @@
+<?php
+require_once "../db.php";
+
+/**
+ * For touching the Imported_Order MySQL Database
+ */
+class Imported_Order extends db {
+  public function get_order($data){
+    if ($data == 'ALL') {
+      $sql = "SELECT * FROM Imported_Order ORDER BY id DESC"
+      $stmt = $this->conn->prepare($sql);
+      $stmt->execute();
+      $result = $stmt->get_result();
+      return $result;
+    } else {
+      // code...
+    }
+  }
+}
