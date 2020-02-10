24a6fc730db9f37e9359128b92ade0ac2ac8115b
dcargs <cargilldevin@gmail.com>
2020-02-10 00:28:33 -0600

    first commit

--- api/imported_order/index.php
+++ api/imported_order/index.php
@@ -4,16 +4,21 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($method) {
   case 'PUT':
+    require_once "order.php";
 
     break;
   case 'POST':
+    require_once "order.php";
 
     break;
   case 'GET':
-
+    require_once "order.php";
+    $imported_order = new Imported_Order;
+    echo $imported_order->get_order('ALL');
     break;
   case 'DELETE':
-    // code...
+    require_once "order.php";
+
     break;
   default:
     http_response_code(405);
